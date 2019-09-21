ddf31127bfc3e34deceed88e82332b3a6cfe6b65
Divya Sasidharan <hello@shortdiv.com>
2019-09-21 14:34:26 -0500

    everything

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,88 +1,12 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="grid">
+    <div class="front" :style="{ backgroundImage: `url('${img}')` }"></div>
+    <div class="back">
+      <div class="postcard">
+        <textarea id="" v-model="message"></textarea>
+        <button>send</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -91,24 +15,70 @@ export default {
   name: "HelloWorld",
   props: {
     msg: String
+  },
+  data() {
+    return {
+      message: "",
+      img:
+        "http://mars.jpl.nasa.gov/msl-raw-images/msss/01000/mcam/1000ML0044631200305217E01_DXXX.jpg"
+    };
+  },
+  mounted() {
+    // fetch("/.netlify/functions/fetch-image")
+    // .then(res => {
+    //   return res.json()
+    // }).then(data => {
+    //   console.log(data)
+    // })
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  height: 90vh;
+  background-color: beige;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.front {
+  background: orangered;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.back {
+  position: relative;
+  left: -15vw;
+  top: 22vh;
 }
-a {
-  color: #42b983;
+.postcard {
+  background: url("https://mdn.mozillademos.org/files/4151/background.jpg");
+  width: 100%;
+  height: 100%;
+  position: relative;
+  background-size: contain;
+  background-repeat: no-repeat;
+}
+#grid > div {
+  padding: 20px;
+  font-size: 4vw;
+  color: white;
+}
+
+@media (max-width: 40em) {
+  .grid {
+    // grid-template-columns: 1fr;
+    // grid-template-rows: 1fr;
+  }
+  .back {
+    // grid-row: 2;
+  }
+}
+textarea {
+  margin-top: 10px;
+  padding: 10px;
+  font-size: 0.7em;
+  background: transparent;
+  line-height: 1.5em;
 }
 </style>
